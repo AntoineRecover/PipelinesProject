fbc819c007510bee8ab174655594d5d86234bc25
AntoineRecover <108389208+AntoineRecover@users.noreply.github.com>
2022-07-03 17:52:17 +0200

    test

--- Python/CleanAMI.py
+++
@@ -1,9 +0,0 @@
-import sys
-import boto.ec2
-
-def ec2_deregister_ami(ec2):
-	images = ec2.get_all_images(owners="self", filters = {'name':str(sys.argv[2])})
-	ec2.deregister_image(i.id, delete_snapshot=True)
-
-ec2 = boto.ec2.connect_to_region(str(sys.argv[1]))
-ec2_deregister_ami(ec2)
